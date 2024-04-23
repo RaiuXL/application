a5e327146f520741e2d3ecf9918fba6e87174988
Nathan Tapia-Ramirez <ntapiaramirez@outlook.com>
2024-04-23 00:43:58 -0400

    Created Summary Page, Updated Controller to handle form validation and Retouch html files

--- index.php
+++ index.php
@@ -45,6 +45,7 @@ $F3->route('GET|POST /info', function($F3){
     $view = new Template();
     echo $view->render('views/info.html');
 });
+
 //experience route
 $F3->route('GET|POST /experience', function($F3){
     if($_SERVER['REQUEST_METHOD']=='POST'){
@@ -63,13 +64,78 @@ $F3->route('GET|POST /experience', function($F3){
             echo '<p>Error</p>';
         }
     }
-    var_dump($F3->get('SESSION'));
+    /*var_dump($F3->get('SESSION'));*/
     $view = new Template();
     echo $view->render('views/experience.html');
 });
+
+//mailing list route
 $F3->route('GET|POST /mailingList', function($F3){
-    var_dump($F3->get('SESSION'));
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        // Initialize an array to store selected checkboxes
+        $selectedCheckboxes = [];
+
+        // Checkboxes for Software Development Jobs
+        if(isset($_POST['JavaScript'])) {
+            $selectedCheckboxes[] = $_POST['JavaScript'];
+        }
+        if(isset($_POST['HTML'])) {
+            $selectedCheckboxes[] = $_POST['HTML'];
+        }
+        if(isset($_POST['PHP'])) {
+            $selectedCheckboxes[] = $_POST['PHP'];
+        }
+        if(isset($_POST['CSS'])) {
+            $selectedCheckboxes[] = $_POST['CSS'];
+        }
+        if(isset($_POST['Java'])) {
+            $selectedCheckboxes[] = $_POST['Java'];
+        }
+        if(isset($_POST['ReactJS'])) {
+            $selectedCheckboxes[] = $_POST['ReactJS'];
+        }
+        if(isset($_POST['Python'])) {
+            $selectedCheckboxes[] = $_POST['Python'];
+        }
+        if(isset($_POST['NodeJS'])) {
+            $selectedCheckboxes[] = $_POST['NodeJS'];
+        }
+
+        // Checkboxes for Industry Verticals
+        if(isset($_POST['SaaS'])) {
+            $selectedCheckboxes[] = $_POST['SaaS'];
+        }
+        if(isset($_POST['Industrial_Tech'])) {
+            $selectedCheckboxes[] = $_POST['Industrial_Tech'];
+        }
+        if(isset($_POST['Health_Tech'])) {
+            $selectedCheckboxes[] = $_POST['Health_Tech'];
+        }
+        if(isset($_POST['Cybersecurity'])) {
+            $selectedCheckboxes[] = $_POST['Cybersecurity'];
+        }
+        if(isset($_POST['Ag_Tech'])) {
+            $selectedCheckboxes[] = $_POST['Ag_Tech'];
+        }
+        if(isset($_POST['HR_Tech'])) {
+            $selectedCheckboxes[] = $_POST['HR_Tech'];
+        }
+
+        // Save the selected checkboxes to session
+        $F3->set('SESSION.selectedCheckboxes', $selectedCheckboxes);
+
+        // Redirect to the next page
+        $F3->reroute("summary");
+    }
+
     $view = new Template();
     echo $view->render('views/mailingList.html');
+});
+
+//summary route
+$F3->route('GET|POST /summary', function($F3){
+    /*var_dump($F3->get('SESSION'));*/
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 $F3->run();
